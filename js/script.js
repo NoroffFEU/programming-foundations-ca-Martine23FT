97c64f6b0ba76a6019d8786451b4599b3c751067
Martine23FT <martinemidttveit@hotmail.com>
2023-06-01 23:11:12 +0200

    Changes

--- js/script.js
+++ js/script.js
@@ -3,18 +3,26 @@
 var tv = {
     brand: "Samsung",
     model: "The Frame",
+    isSmartTv: true,
 
     sizes: {
         size: 45,
         size: 65,
         size: 72,
         size: 85,
+    },
+
+    colors: {
+        color1: "Black",
+        color2: "Dark Oak",
+        color3: "Light Oak",
+        color4: "White",
     }
 } 
 
-console.log(iphone);
+console.log(tv);
 
-// IF & ELSE //
+// IF & ELSE - Lage dette om i forhold til fargevalg og størrelse? //
 
 var inStock = true;
 
