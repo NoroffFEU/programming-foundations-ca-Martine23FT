58568e8a0c94f5716e5d66a405428594717dd8e1
Martine23FT <martinemidttveit@hotmail.com>
2023-06-01 22:52:00 +0200

    Changes

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// IPHONE //
+
+var tv = {
+    brand: "Samsung",
+    model: "The Frame",
+
+    sizes: {
+        size: 45,
+        size: 65,
+        size: 72,
+        size: 85,
+    }
+} 
+
+console.log(iphone);
+
+// IF & ELSE //
+
+var inStock = true;
+
+if (inStock === true) {
+    console.log("In stock");
+}
+else {
+    console.log("NOT in stock");
+}
+
+console.log(inStock);
+
+// FUNCTION ON BUTTON //
+
+
+
