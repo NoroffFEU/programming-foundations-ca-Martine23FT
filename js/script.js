7aaee501709353423d763e871fd89fa63981dea0
Martine23FT <martinemidttveit@hotmail.com>
2023-06-03 19:20:40 +0200

    Changes

--- js/script.js
+++ js/script.js
@@ -4,41 +4,29 @@ var tv = {
     brand: "Samsung",
     model: "The Frame",
     isSmartTv: true,
-    colors: ["Black", "Dark Oak", "Light Oak", "White"],
     inches: [45, 65, 72, 85],
+    colors: ["Black", "Dark Oak", "Light Oak", "White"],
     on: false,
+    inStock: true,
+
+    power: function () {
+        tv.on = !tv.on;
+        document.getElementById("on").textContent = tv.on ? "On" : "Off";
+    },
 } 
 
 console.log(tv);
+
 document.getElementById("brand").innerHTML = tv.brand;
+document.getElementById("model").innerHTML = tv.model;
+document.getElementById("color").innerHTML = tv.colors;
+
+var powerBtn = document.getElementById("power");
+powerBtn.addEventListener("click", function () {
+tv.power(); })
 
 
-var ignitionButton = document.getElementById("ignition");
-ignitionButton.addEventListener("click", function () {
-    tv.ignition();
-    document.getElementById("on").textContent = tv.on ? "On" : "Off";
-});
 
 
 
-document.querySelector("h2").innerHTML = "Its weekend"
-// IF & ELSE - Lage dette om i forhold til fargevalg og størrelse? //
-
-var tvOption = {
-    color: "Black",
-    size: 45,
-    inStock: true
-}
-
-if (inStock === true) {
-    console.log("In stock");
-}
-
-else {
-    console.log("NOT in stock");
-}
-
-console.log(inStock);
-
-// FUNCTION ON BUTTON //
 
