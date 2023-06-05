5c83026b334eab94b88e26dd09da85a9cca20a8b
Martine23FT <martinemidttveit@hotmail.com>
2023-06-05 16:32:11 +0200

    Changes

--- js/script.js
+++ js/script.js
@@ -1,23 +1,5 @@
-//HEADER//
 
-var toggle = document.getElementById('navbar-toggle');
-var navbar = document.getElementById('navbar-nav');
-
-if(navbar) {
-    navbar.addEventListener('click', () => {
-        navbar.classList.remove('show')
-        toggle.classList.remove('show')
-    })
-}
-
-if(toggle) {
-    toggle.addEventListener('click', () => { 
-        navbar.classList.toggle('show');
-        toggle.classList.toggle('show');
-    })
-}
-
-//OBJECT//
+// OBJECT //
 
 var tv = {
     brand: "Samsung",
@@ -26,62 +8,48 @@ var tv = {
     inches: [45, 65, 72, 80],
     on: false,
     inStock: true,
-    price: 10.000,
-
-    color1: "Black",
-    color2: "Dark Oak",
-    color3: "Light Oak",
-    color4: "white",
-
+    price: [10000, 12399, 13499, 14999],
+    colors:["Black", "Dark Oak", "Light Oak", "White"],
+    
     power: function () {
         tv.on = !tv.on;
         document.getElementById("on").textContent = tv.on ? "On" : "Off";
     },
-
-    colorOne: function () {
-        tv.color1 = !tv.color1;
-        document.getElementById("color").textContent = tv.color1 ? "Black" : "Black";
-    },
-
-    colorTwo: function () {
-        tv.color2 = !tv.color2;
-        document.getElementById("color").textContent = tv.color2 ? "Dark Oak" : "Dark Oak";
-    },
-
-    colorTree: function () {
-        tv.color3 = !tv.color3;
-        document.getElementById("color").textContent = tv.color3 ? "Light Oak" : "Light Oak";
-    },
-
-    colorFour: function () {
-        tv.color4 = !tv.color4;
-        document.getElementById("color").textContent = tv.color4 ? "White" : "White";
-    },
 }
-
-//Possible to do the same with inches? //
 
 console.log(tv);
 
 document.getElementById("brand").innerHTML = tv.brand;
 document.getElementById("model").innerHTML = tv.model;
 
-var colorBtn1 = document.getElementById("color1");
-colorBtn1.addEventListener("click", function () {
-tv.colorOne(); })
 
-var colorBtn2 = document.getElementById("color2");
-colorBtn2.addEventListener("click", function () {
-tv.colorTwo(); })
+// Buttons //
 
-var colorBtn3 = document.getElementById("color3");
-colorBtn3.addEventListener("click", function () {
-tv.colorTree(); })
+function displayColorName(elementID, number){
+    var colorBtn = document.getElementById(elementID);
+    colorBtn.addEventListener("click", function () {
+    document.getElementById("color").textContent = tv.colors[number];
+    });  
+}
 
-var colorBtn4 = document.getElementById("color4");
-colorBtn4.addEventListener("click", function () {
-tv.colorFour(); })
+displayColorName("color1", 0);
+displayColorName("color2", 1);
+displayColorName("color3", 2);
+displayColorName("color4", 3);
 
 var powerBtn = document.getElementById("power");
 powerBtn.addEventListener("click", function () {
-tv.power(); })
+tv.power(); 
+});
+
+function displayTvPrice(size, number){
+    var sizeBtn = document.getElementById(size);
+    sizeBtn.addEventListener("click", function () {
+    document.getElementById("price").textContent = tv.price[number];
+    });  
+}
+
+displayTvPrice("size1", 0);
+displayTvPrice("size2", 1);
+displayTvPrice("size3", 2);
+displayTvPrice("size4", 3);
