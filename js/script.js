132f8be30eaab45b9f6bda2c3301b2dfacdedb8e
Martine23FT <martinemidttveit@hotmail.com>
2023-06-06 14:05:30 +0200

    Changes

--- js/script.js
+++ js/script.js
@@ -6,14 +6,40 @@ var tv = {
     model: "The Frame",
     isSmartTv: true,
     inches: [45, 65, 72, 80],
-    on: false,
     inStock: true,
     price: ["10.000 kr", "12.399 kr", "13.499 kr", "14.999 kr"],
     colors:["Black", "Dark Oak", "Light Oak", "White"],
-}
+    options: 
+    [
+        {
+            name: "Option 1",
+            screenSize: 45,
+            inStock: 3,
+            colour: "Black"     
+        },
 
-var inStock = true;
+        {
+            name: "Option 2",
+            screenSize: 65,
+            inStock: 5,
+            colour: "Dark Oak"
+        },
 
+        {
+            name: "Option 3",
+            screenSize: 72,
+            inStock: 11,
+            colour: "Light Oak"
+        },
+
+        {
+            name: "Option 4",
+            screenSize: 80,
+            inStock: 17,
+            colour: "White"
+        }
+    ]
+} 
 
 console.log(tv);
 
@@ -62,4 +88,16 @@ buttonColor ("size2")
 buttonColor ("size3")
 buttonColor ("size4")
 
-///// Possible to make a if / else for the quantity, with in stock and not in stock. 
+///tvOption1 = {
+    ///screenSize: 45,
+    ///colour: "Black",
+    ///inStock: 3,
+///}
+
+///if (inStock < 3) {
+    ///document.getElementById("availability").textContent = "IN STOCK";
+///}
+
+///else {
+    ///document.getElementById("availability").textContent = "NOT IN STOCK";
+///}
